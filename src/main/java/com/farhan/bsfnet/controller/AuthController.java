ac74327cdbe45a2e43f29d74b5cf94df4462e7d8
rifqiimamr <imamramadhan.rifqi@gmail.com>
2023-12-20 15:56:09 +0700

    nyoba han

--- src/main/java/com/farhan/bsfnet/controller/AuthController.java
+++ src/main/java/com/farhan/bsfnet/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
+//sad
     @Autowired
     private AuthService authService;
 
